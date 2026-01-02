8223acc76bd6808f86aa0c58bf55e94ba07cb9cc
itssunnyfunny <sunnyfunny4062@gmail.com>
2026-01-02 10:46:29 +0530

    adding export file for ai contracts and readers

---
+++ ai/index.ts
@@ -0,0 +1,9 @@
+// ai/index.ts
+
+export * from "./readers/branch.reader"
+export * from "./readers/org.reader"
+export * from "./readers/payment.reader"
+
+export * from "./contracts/branch.contract"
+export * from "./contracts/org.contract"
+export * from "./contracts/payment.contract"
