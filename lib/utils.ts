fd29fc46957524c3462a3f578f13d791db87d85b
itssunnyfunny <sunnyfunny4062@gmail.com>
2026-02-04 16:33:36 +0530

    for merging classes : i forgot to commit it

---
+++ lib/utils.ts
@@ -0,0 +1,7 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+}
+// Utility for merging tailwind classes
